80222a40954e274ddc541d803bd7a5faec898d33
manoj <manoj.sarwate@ssiedu.in>
2019-05-21 15:39:37 +0530

    21-05-19

---
+++ src/main/java/com/ssi/StudentEntry.java
@@ -0,0 +1,46 @@
+package com.ssi;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+
+public class StudentEntry {
+
+	public static void main(String[] args) {
+
+		//enter the records of some students.
+		
+		Student s1=new Student(111,"AAA","MECH",1);
+		Student s2=new Student(112,"BBB","IT",2);
+		Student s3=new Student(113,"CCC","EC",4);
+		Student s4=new Student(114,"DDD","CIVIL",6);
+		Student s5=new Student(115,"EEE","CS",3);
+		Student s6=new Student(116,"FFF","IT",2);
+		
+		Configuration conf=new Configuration().configure();
+		SessionFactory sessionFactory=	conf.buildSessionFactory();
+		Session session=sessionFactory.openSession();	
+		
+		Transaction tr=session.beginTransaction();
+		
+		
+		session.save(s1);
+		session.save(s2);
+		session.save(s3);
+		session.save(s4);
+		session.save(s5);
+		session.save(s6);
+		tr.commit();
+		
+		session.close();
+		System.out.println("RECORDS ADDED");		
+		
+		
+		
+
+	}
+
+}
